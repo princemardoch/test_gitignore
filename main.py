e012bd237d56bb833ba972b9c1714ff92206e776
leprincemardoch <doublepo56@gmail.com>
2024-09-02 23:33:33 +0000

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-with sqlite3.connect('dbs/test.db') as conn:
+with sqlite3.connect('db/test.db') as conn:
     cursor = conn.cursor()
     cursor.execute(''' CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT, test TEXT ) ''')
-    conn.commit()
+    conn.commit() 
